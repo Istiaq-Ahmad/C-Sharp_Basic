d213835127803a93b15a5f16aff776466756826f
Istiaq Ahmad <108044430+Istiaq-Ahmad@users.noreply.github.com>
2024-03-05 13:17:51 +0600

    Create User Input and Printing.cs

---
+++ User Input and Printing.cs
@@ -0,0 +1,14 @@
+
+Console.WriteLine("User Input and Output\n");
+
+string variable; //defining a variable with data type
+Console.Write("Enter your name: "); 
+
+variable = Console.ReadLine(); //taking input from users
+
+Console.WriteLine("\nWelcome to C# Basic " + variable); //printing the varible using +
+
+//another way of of using variables inside the printing statement
+Console.WriteLine($"Hope you are doing well {variable}"); //printing the variable using $ 
+
+
