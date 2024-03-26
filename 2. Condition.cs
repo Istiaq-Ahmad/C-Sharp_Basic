96e14c4496aac97fdecee2286b0b103cf28f5dcf
Istiaq Ahmad <108044430+Istiaq-Ahmad@users.noreply.github.com>
2024-03-27 00:38:07 +0600

    Create 2. Condition.cs

---
+++ 2. Condition.cs
@@ -0,0 +1,13 @@
+int time = 22;
+
+if (time < 10){
+  Console.WriteLine("Good morning.");
+}
+
+else if (time < 20){
+  Console.WriteLine("Good day.");
+} 
+
+else {
+  Console.WriteLine("Good evening.");
+}
