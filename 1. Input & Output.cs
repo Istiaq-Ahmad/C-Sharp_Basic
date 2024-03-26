49235bf6cb165a92d4801e51d1dcc625fe2db418
Istiaq Ahmad <108044430+Istiaq-Ahmad@users.noreply.github.com>
2024-03-27 00:35:30 +0600

    Update 1. Input & Output.cs

--- 1. Input & Output.cs
+++ 1. Input & Output.cs
@@ -1,6 +1,3 @@
-
-Console.WriteLine("User Input and Output\n");
-
 string variable; //defining a variable with data type
 
 Console.Write("Enter your name: "); 
@@ -9,6 +6,7 @@ variable = Console.ReadLine(); //taking input from users
 Console.WriteLine("\nWelcome to C# Basic " + variable); //printing the varible using +
 
 //another way of of using variables inside the printing statement
+
 Console.WriteLine($"Hope you are doing well {variable}"); //printing the variable using $ 
 
 
