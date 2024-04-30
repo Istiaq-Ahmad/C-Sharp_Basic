cbfbb2f980e8e94bcd495aed4bdaaa8b79bd3b7d
Istiaq Ahmad <108044430+Istiaq-Ahmad@users.noreply.github.com>
2024-04-30 15:29:41 +0600

    Update and rename 4. For Loop.cs to 4. For Loop & For Each.cs

---
+++ 4. For Loop & For Each.cs
@@ -0,0 +1,12 @@
+using System;
+
+for (int i = 0; i <= 5; i = i + 1) 
+{
+  Console.WriteLine(i);
+}
+
+string[] language = {"Python", "C#", "C++", "Java"};
+foreach (string i in language) 
+{
+  Console.WriteLine(i);
+}
--- 4. For Loop.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-for (int i = 0; i <= 5; i = i + 1) 
-{
-  Console.WriteLine(i);
-}
