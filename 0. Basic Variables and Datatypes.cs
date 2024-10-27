b149964857da26b085c57a3a985670ca26ec8fad
Istiaq Ahmad <108044430+Istiaq-Ahmad@users.noreply.github.com>
2024-10-27 10:49:07 +0600

    Update 0. Basic Variables and Datatypes.cs

--- 0. Basic Variables and Datatypes.cs
+++ 0. Basic Variables and Datatypes.cs
@@ -10,8 +10,10 @@ namespace MyApplication
       int age = 30;
       float height = 5.6F;
       double salary = 4000.500;
+      bool trueorfalse = true;
+      char letter = 'J';
       
-      Console.WriteLine($"{name} is a {age} years old person with {height} feet height and has a salary of {salary} USD per month.");  
+      Console.WriteLine($"{name} is a {age} years old person with {height} feet height and has a salary of {salary} USD per month. The char type data enclosed under single qoutetion like '{letter}'. The expresions can be checked if true or false with bool data type like: {trueorfalse}.");
      }
   }
 }
