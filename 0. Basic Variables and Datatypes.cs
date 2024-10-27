c41d30599b92a1c9d89325a42ddae656bafdd155
Istiaq Ahmad <108044430+Istiaq-Ahmad@users.noreply.github.com>
2024-10-27 10:13:40 +0600

    Update 0. Basic Variables and Datatypes.cs

--- 0. Basic Variables and Datatypes.cs
+++ 0. Basic Variables and Datatypes.cs
@@ -7,7 +7,11 @@ namespace MyApplication
     static void Main(string[] args)
     {
       string name = "John";
-      Console.WriteLine(name);  
+      int age = 30;
+      float height = 5.6F;
+      double salary = 4000.500;
+      
+      Console.WriteLine($"{name} is a {age} years old person with {height} feet height and has a salary of {salary} USD per month.");  
      }
   }
 }
